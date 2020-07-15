dceb4f7766d3cb3bbd6e36e5642bc6c2d35dd790
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-07-15 18:35:30 -0400

    working on top bar

--- src/App.js
+++ src/App.js
@@ -34,54 +34,95 @@ class App extends Component {
     let quoteDiv = document.querySelector("#bOne");
     console.log(quoteDiv);
     quoteDiv.classList.toggle("display");
+    fetch(res, options)
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          newQuote: res,
+          dataReady: true,
+        });
+      });
   };
   handleFClick = (e) => {
     e.preventDefault();
     let quoteDiv = document.querySelector("#bTwo");
     console.log(quoteDiv);
     quoteDiv.classList.toggle("display");
+    fetch(foodRes, options)
+      .then((foodRes) => {
+        return foodRes.json();
+      })
+      .then((foodRes) => {
+        console.log(foodRes);
+        this.setState({
+          foodQuote: foodRes[Math.floor(Math.random() * foodRes.length)],
+          dataReady: true,
+        });
+      });
   };
   handleGClick = (e) => {
     e.preventDefault();
     let quoteDiv = document.querySelector("#bThree");
     console.log(quoteDiv);
     quoteDiv.classList.toggle("display");
+    fetch(govRes, options)
+      .then((govRes) => {
+        return govRes.json();
+      })
+      .then((govRes) => {
+        console.log(govRes);
+        this.setState({
+          govQuote: govRes[Math.floor(Math.random() * govRes.length)],
+          dataReady: true,
+        });
+      });
   };
   handleAClick = (e) => {
     e.preventDefault();
     let quoteDiv = document.querySelector("#bFour");
     console.log(quoteDiv);
     quoteDiv.classList.toggle("display");
+    fetch(alcRes, options)
+      .then((alcRes) => {
+        return alcRes.json();
+      })
+      .then((alcRes) => {
+        console.log(alcRes);
+        this.setState({
+          alcQuote: alcRes[Math.floor(Math.random() * alcRes.length)],
+          dataReady: true,
+        });
+      });
   };
 
-  /*handleClick = (e) => {
-    e.preventDefault();
-    let quoteDiv = document.querySelectorAll(".button");
-    console.log(quoteDiv);
-    for (let i = 0; i < quoteDiv.length; i++) {
-      quoteDiv.classList.toggle("display");
-    }
-  };*/
   render() {
     return (
-      <div className="App">
-        <div className="accordianDiv">
-          <button className="button" onClick={this.handleRClick}>
-            Random Quote
-          </button>
-          <ButtonOne newQuote={this.state.newQuote} />
-          <button className="button" onClick={this.handleFClick}>
-            Food Quote
-          </button>
-          <ButtonTwo newFoodQuote={this.state.foodQuote} />
-          <button className="button" onClick={this.handleGClick}>
-            Government Quote
-          </button>
-          <ButtonThree newGovQuote={this.state.govQuote} />
-          <button className="button" onClick={this.handleAClick}>
-            Alcohol Quote
-          </button>
-          <ButtonFour newAlcQuote={this.state.alcQuote} />
+      <div className="appBody">
+        <div className="top">
+          <p classname="topTitle">Ron Swanson Quotes:<span>from the Ron Swanson Api</span></p>
+        </div>
+        <div className="App">
+          <div className="accordianDiv">
+            <button className="button" onClick={this.handleRClick}>
+              Random Quote
+            </button>
+            <ButtonOne newQuote={this.state.newQuote} />
+            <button className="button" onClick={this.handleFClick}>
+              Food Quote
+            </button>
+            <ButtonTwo newFoodQuote={this.state.foodQuote} />
+            <button className="button" onClick={this.handleGClick}>
+              Government Quote
+            </button>
+            <ButtonThree newGovQuote={this.state.govQuote} />
+            <button className="button" onClick={this.handleAClick}>
+              Alcohol Quote
+            </button>
+            <ButtonFour newAlcQuote={this.state.alcQuote} />
+          </div>
         </div>
       </div>
     );
@@ -136,3 +177,12 @@ class App extends Component {
 }
 
 export default App;
+
+/*handleClick = (e) => {
+    e.preventDefault();
+    let quoteDiv = document.querySelectorAll(".button");
+    console.log(quoteDiv);
+    for (let i = 0; i < quoteDiv.length; i++) {
+      quoteDiv.classList.toggle("display");
+    }
+  };*/

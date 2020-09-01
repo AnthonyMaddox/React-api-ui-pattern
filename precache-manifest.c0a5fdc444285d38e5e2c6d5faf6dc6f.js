b26439cdc6a2a600ccbfa50458d18ab2fada4b96
AnthonyMaddox <anthonyjmaddox@gmail.com>
2020-09-01 12:48:41 -0400

    Updates

---
+++ precache-manifest.c0a5fdc444285d38e5e2c6d5faf6dc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cfc428af33b33d7874f3eef15c27e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5696b8b09b52eea354c8",
+    "url": "/static/css/main.54c12a3a.chunk.css"
+  },
+  {
+    "revision": "530a4a570d1ebf35402f",
+    "url": "/static/js/2.8870f1c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8870f1c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5696b8b09b52eea354c8",
+    "url": "/static/js/main.77e6120e.chunk.js"
+  },
+  {
+    "revision": "83f015969b61e186121e",
+    "url": "/static/js/runtime-main.8aefa227.js"
+  }
+]);
